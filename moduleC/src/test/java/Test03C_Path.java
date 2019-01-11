e7a1d4fcda1955a91ed93b354e1579102a193b5a
trinhh <thevency@gmail.com>
2019-01-11 15:02:20 +0700

    updated test03C
    Signed-off-by: trinhh <thevency@gmail.com>

---
+++ moduleC/src/test/java/Test03C_Path.java
@@ -0,0 +1,10 @@
+import com.report.core.Listen;
+import org.testng.annotations.Test;
+
+public class Test03C_Path extends Listen {
+    @Test
+    public void testCurrentPathC(){
+        System.out.println("======= Test03C_Path =======");
+//        SetPath.getCurrentDir("..\\moduleB\\src\\test\\picture.jpg");
+    }
+}
